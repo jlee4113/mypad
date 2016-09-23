29e3b16e5e7ae5ed34585f7b89f5dff283c62c47
leeek62 <leeek62@gmail.com>
2016-09-23 10:43:26 -0500

    PHP Updates

--- php/Processes/validateEmail.php
+++ php/Processes/validateEmail.php
@@ -3,33 +3,34 @@
   include('../utilities/globals.php');
   header('Access-Control-Allow-Origin: *');
 
-<<<<<<< HEAD
+
 //Declarations
 $return = new json();
 $response = array();
-=======
-  //Declarations
-  $return = new json();
->>>>>>> 9d255ebc66c0f8b3cdda8659698a394bc5b019bb
 
-  // Get Parameters
-  $email     = get_variable('primEmail', $_POST);
-
-  //Make sure primary e-mail is set
-  if (empty($email)) {
-    $return->returnCode = '8';
-    $return->messages = add_to_array("message","Email Empty",$return->messages);
-    echo json_encode($return);
-    exit;
-  }
+//Declarations
+$return = new json();
 
 
-<<<<<<< HEAD
+// Get Parameters
+$email     = get_variable('primEmail', $_POST);
+
+//Make sure primary e-mail is set
+if (empty($email)) {
+  $return->returnCode = '8';
+  $return->messages = add_to_array("message","Email Empty",$return->messages);
+  echo json_encode($return);
+  exit;
+}
+
+
+
 //Check if exists
- $table = 'users';
- $where = add_where("primEmail", $email, $where = array());
- $fields = "*";
- $response = select_from_table($table, $fields, $where);
+
+$table = 'users';
+$where = add_where("primEmail", $email, $where = array());
+$fields = "*";
+$response = select_from_table($table, $fields, $where);
 
 // Then add the user to the person table with only the e-mail address
 if (empty(json_decode($response, true))) {
@@ -37,26 +38,9 @@ if (empty(json_decode($response, true))) {
   $return->messages = add_to_array("message","Email does not exist",$return->messages);
 } else {
   $return->returnCode = '2';
-  $return->messages = add_to_array("message","Email exists",$return->messages);  
-  $return->data = $response;
+  $return->messages = add_to_array("message","Email exists",$return->messages);
+  $return->data = json_decode($response);
 }
 echo json_encode($return);
-=======
-  //Check if exists
-   $table = 'users';
-   $where = add_where("primEmail", $email, $where = array());
-   $fields = "*";
-   $response = select_from_table($table, $fields, $where);
 
-  // Then add the user to the person table with only the e-mail address
-  if (empty(json_decode($response, true))) {
-    $return->returnCode = '1';
-    $return->messages = add_to_array("message","Email does not exist",$return->messages);
-  } else {
-    $return->returnCode = '2';
-    $return->messages = add_to_array("message","Email exists",$return->messages);
-    $return->data = json_decode($response);
-  }
-  echo json_encode($return);
->>>>>>> 9d255ebc66c0f8b3cdda8659698a394bc5b019bb
 ?>
